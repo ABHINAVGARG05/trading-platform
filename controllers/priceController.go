1e43ef6d5dbf309f480a0ce2f0616629d990368c
ABHINAVGARG05 <abhichhrp@gmail.com>
2024-10-25 21:37:42 +0530

    new price updater mechanism

--- controllers/priceController.go
+++ controllers/priceController.go
@@ -2,6 +2,7 @@ package controllers
 
 import (
 	"time"
+	"math/rand"
 
 	"github.com/ABHINAVGARG05/trading-platform/database"
 	"github.com/ABHINAVGARG05/trading-platform/models"
@@ -22,11 +23,31 @@ func PriceUpdater(){
 	}
 }
 
+func RandomPriceUpdater(stock *models.Stock){
+	rand.Seed(time.Now().UnixNano())
+	randomNum := rand.Intn(2) + 1
+
+	switch randomNum {
+	case 1:
+		dmr := float64(stock.Demand)/float64(stock.Supply+1)
+		stock.Price += stock.Price*0.01*dmr;
+	case 2:
+		dmr := float64(stock.Supply)/float64(stock.Demand+1)
+		stock.Price -= stock.Price*0.01*dmr
+	}
+	database.DB.Save(&stock)
+}
+
+
 func PriceTicker(){
 	ticker := time.NewTicker(1*time.Minute)
 	go func(){
 		for range ticker.C{
-			PriceUpdater()
+			var stocks[] models.Stock
+			database.DB.Find(&stocks)
+			for _, stock := range stocks {
+                RandomPriceUpdater(&stock)
+            }
 		}
 	}()
 }
