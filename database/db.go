fbba2710b2cbc345300715227f9004cd1cbb88d3
ABHINAVGARG05 <abhichhrp@gmail.com>
2024-10-26 00:05:23 +0530

    added mechanism for seeding stocks in db

--- database/db.go
+++ database/db.go
@@ -5,6 +5,8 @@
         "gorm.io/driver/postgres"
         "gorm.io/gorm"
         "github.com/ABHINAVGARG05/trading-platform/models"
+        "time"
+        "math/rand"
     )
 
     var DB *gorm.DB
@@ -18,4 +20,22 @@
         }
 
         DB.AutoMigrate(&models.User{}, &models.Stock{}, &models.Portfolio{},&models.Order{})
+
+    rand.Seed(time.Now().UnixNano())
+
+    for i := 0; i < 100; i++ {
+        stock := models.Stock{
+            Symbol:    "STK" + string(i+1),
+            Price:     rand.Float64() * 100, 
+            Volume:    10000,
+            Supply:    uint(rand.Intn(500000)), 
+            Demand:    uint(rand.Intn(500000)), 
+            CreatedAt: time.Now(),
+            UpdatedAt: time.Now(),
+        }
+
+        if err := DB.Create(&stock).Error; err != nil {
+            log.Println("Failed to insert stock:", err)
+        }
+    }
     }
